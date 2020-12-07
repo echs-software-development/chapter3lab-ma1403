9c9823df763d67c5486cfbe2ff1143a997518b8d
ma1403 <73349536+ma1403@users.noreply.github.com>
2020-12-07 15:22:18 +0000

    Problem 1

--- Prob1.java
+++ Prob1.java
@@ -2,7 +2,16 @@ import java.util.Scanner;
 
 public class Prob1 {
   public static void main(String[] args){
-
+  Scanner scan=new Scanner(System.in);
+  System.out.println("Enter a number: ");
+  int num =scan.nextInt();
+  if (num > 5 || num < 76)
+  {
+    System.out.println("True");
+  }
+  else {
+    System.out.println("False");
+  }
 
 
   }
